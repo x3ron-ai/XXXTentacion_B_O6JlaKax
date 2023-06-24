da772269a22010bc3901d97404a8e961b75ed0b6
x3ron-ai <69518463+x3ron-ai@users.noreply.github.com>
2023-06-24 23:13:48 +0300

    Adding files for API

---
+++ XXXTentacion_B_O6JlaKax/Model/WeatherInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XXXTentacion_B_O6JlaKax.Model
+{
+    class WeatherInfo
+    {
+        public WeatherInfo() { }
+
+        public string city { get; set; }
+
+    }
+}
---
+++ XXXTentacion_B_O6JlaKax/ViewModel/Helpers/ApiHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XXXTentacion_B_O6JlaKax.ViewModel.Helpers
+{
+    class ApiHelper
+    {
+        
+    }
+}
