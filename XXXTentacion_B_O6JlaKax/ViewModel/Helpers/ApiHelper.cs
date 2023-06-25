a00d47a30761861a6ce61f27473a478c7f82de1d
x3ron-ai <69518463+x3ron-ai@users.noreply.github.com>
2023-06-25 13:07:09 +0300

    Get city weather API realisation

--- XXXTentacion_B_O6JlaKax/ViewModel/Helpers/ApiHelper.cs
+++ XXXTentacion_B_O6JlaKax/ViewModel/Helpers/ApiHelper.cs
@@ -1,13 +1,24 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace XXXTentacion_B_O6JlaKax.ViewModel.Helpers
 {
-    class ApiHelper
+    static class ApiHelper
     {
-        
+        public static JObject getWeather(string city)
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=cfcef6872ef1ebf9b1683779749b649d&lang=ru&units=metric");
+            HttpResponseMessage response = httpClient.Send(request);
+
+            return JsonConvert.DeserializeObject<JObject>(response.Content.ReadAsStringAsync().Result);
+        }
     }
 }
